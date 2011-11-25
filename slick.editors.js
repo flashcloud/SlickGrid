f25432f6fad924548cfc8c97f2aa545177650836
flashcloud <flashcloud163@gmail.com>
2011-11-25 15:44:35 +0800

    Add NumberCellEditor

--- slick.editors.js
+++ slick.editors.js
@@ -155,6 +155,71 @@
             this.init();
         },
 
+        NumberCellEditor : function(args) {
+            var $input;
+            var defaultValue;
+            var scope = this;
+
+            this.init = function() {
+                $input = $("<INPUT type=text class='editor-text' />");
+
+                $input.bind("keydown.nav", function(e) {
+                    if (e.keyCode === $.ui.keyCode.LEFT || e.keyCode === $.ui.keyCode.RIGHT) {
+                        e.stopImmediatePropagation();
+                    }
+                });
+
+                $input.appendTo(args.container);
+                $input.focus().select();
+            };
+
+            this.destroy = function() {
+                $input.remove();
+            };
+
+            this.focus = function() {
+                $input.focus();
+            };
+
+            this.loadValue = function(item) {
+                defaultValue = item[args.column.field];
+                $input.val(defaultValue);
+                $input[0].defaultValue = defaultValue;
+                $input.select();
+            };
+
+            this.serializeValue = function() {
+                return parseInt($input.val(),10) || 0;
+            };
+
+            this.applyValue = function(item,state) {
+                item[args.column.field] = state;
+            };
+
+            this.isValueChanged = function() {
+                return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
+            };
+
+            this.validate = function() {
+                if (isNaN($input.val()))
+                    return {
+                        valid: false,
+                        msg: "Please enter a valid integer"
+                    };
+
+                return {
+                    valid: true,
+                    msg: null
+                };
+            };
+
+            this.getCell = function(){
+              return $input.parent();
+            };
+
+            this.init();
+        },
+
         IntegerCellEditor : function(args) {
             var $input;
             var defaultValue;
@@ -1047,6 +1112,9 @@
             var scope = this;
             var autocompleteActive = false;
             var editorContext;
+
+            if (!args.column.editorOptions)
+                throw  'must define editorOptions property in column config';
 
             var $container = $(args.container);
 
